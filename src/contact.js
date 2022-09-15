13e0b685974977121be5ca12a8ead90769f2b51b
Stewart Gliniecki <stewgliniecki@gmail.com>
2022-09-15 18:12:10 +0100

    Menu page & Contact page

--- src/contact.js
+++ src/contact.js
@@ -1,9 +1,32 @@
-
+import './styles.css';
 
 function contact() {
     const contentBox = document.querySelector('.contentBox')
 
     contentBox.textContent = '';
+    const contactdiv = document.createElement('div');
+    const contactp = document.createElement('p');
+    const contactb = document.createElement('p');
+    const contactc = document.createElement('p');
+    const contactd = document.createElement('p');
+    const contacte = document.createElement('p');
+    const contactf = document.createElement('p');
+    contactdiv.classList.add('contactp')
+    contactp.textContent = "Paddys Pizza Palace"
+    contactb.textContent = '54 Saint Martins Close'
+    contactc.textContent = 'Blackfriars';
+    contactd.textContent = 'London'
+    contacte.textContent= '01532 560 450';
+    contactf.textContent = 'PaddysPizzaPalace@bigman.com'
+
+
+    contactdiv.appendChild(contactp)
+    contactdiv.appendChild(contactb)
+    contactdiv.appendChild(contactc)
+    contactdiv.appendChild(contactd)
+    contactdiv.appendChild(contacte)
+    contactdiv.appendChild(contactf)
+    contentBox.appendChild(contactdiv);
 }
 
 export {contact}
--- src/menu.js
+++ src/menu.js
@@ -1,8 +1,46 @@
+import pizza from './images/pizza.jpg';
+import './styles.css'
 
+
+const menuItemFactory = (item, Itemimage, description) =>{
+
+
+    const itemBox = () =>{
+
+        const itemDiv = document.createElement('div')
+        const itemHeader = document.createElement('h1');
+        const image = new Image();
+        const descriptionItem  = document.createElement('p');
+
+
+        itemHeader.textContent = item
+        image.src = Itemimage;
+        image.classList.add('imageOne')
+
+        descriptionItem.textContent = description;
+
+        itemDiv.appendChild(itemHeader)
+        itemDiv.appendChild(image)
+        itemDiv.appendChild(descriptionItem)
+        itemDiv.classList.add('itemDiv')
+
+        return itemDiv
+
+    } 
+
+    return{itemBox}
+}
 
 function menu() {
     const contentBox = document.querySelector('.contentBox')
     contentBox.textContent = '';
+
+    const itemOne = menuItemFactory('Pizza', pizza, 'test')
+    const itemTwo = menuItemFactory('Test', pizza, 'Test')
+    contentBox.appendChild(itemOne.itemBox())
+    contentBox.appendChild(itemTwo.itemBox())
+
+    
 }
 
 export {menu}
