7a1864b5136bac32276f98f854be4628508e3d16
Stewart Gliniecki <stewgliniecki@gmail.com>
2022-09-15 14:29:32 +0100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Home',
+      template : 'src/index.html'
+    }),
+  ],
+};
